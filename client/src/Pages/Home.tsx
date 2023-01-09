f78e5664a90971a339838a49d5ccb30a060f2cde
limdumb <dlaruddls@nate.com>
2023-01-09 13:58:46 +0900

    [FE Style] Event Banner, Top List Banner 기본 프레임 설정

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -1,11 +1,34 @@
-import "./Style/home.css"
+import "./Style/home.css";
+import styled from "styled-components";
+
+const BannerContainer = styled.div<{ height: string }>`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: ${(props) => props.height};
+  background-color: white;
+  margin-bottom: 20px;
+`;
+
+const TopBannerContainer = styled(BannerContainer)`
+  padding: 20px 40px;
+`;
+
+const BannerContent = styled.img<{ width: string }>`
+  width: ${(props) => props.width};
+  height: 100%;
+`;
 
 export default function Home() {
-
-  return(
+  return (
     <div className="Home_Container">
-      <div className="Main_Banner"/>
-      <div className="Event_Banner"/>
+      <BannerContainer height="500px"></BannerContainer>
+      <BannerContainer height="500px">
+        <BannerContent src="" width={"500px"} />
+      </BannerContainer>
+      <TopBannerContainer height="250px">
+        <BannerContent src="" width={"250px"} />
+      </TopBannerContainer>
     </div>
-  )
+  );
 }
