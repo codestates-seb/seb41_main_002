6c92c5170c39f307132cbe64deafe17ac177d6ba
minseob shim <shim5505@gmail.com>
2023-01-09 15:32:49 +0900

    [FE Feat]버튼 클릭에 따른 헤더 변경 기능 추가

--- client/src/Pages/ItemsTopList.tsx
+++ client/src/Pages/ItemsTopList.tsx
@@ -1,21 +1,55 @@
+import React, { useState } from "react";
 import "./Style/ItemsTopList.css";
 
 export default function ItemsTopList() {
+  // 해당 상태의 기본값은 이전 페이지에서 이동할 때 전달받는 방식을 채택 예정
+  // 현재는 첫 번째 카테고리인 "토너"를 기본값을 설정
+  const [category, setCategory] = useState<string>("토너");
+  const changeCategory = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    const button: HTMLButtonElement = event.currentTarget;
+    setCategory(button.name);
+  };
   return (
     <div>
-      <h2 className="Top_List_Title">이 달의 Top 10 </h2>
+      <h2 className="Top_List_Title">이 달의 Top 10 {category}</h2>
       <ul className="Filter_Options">
         <li>
-          <button className="Filter_Button">토너</button>
+          <button
+            className="Filter_Button"
+            onClick={changeCategory}
+            name="토너"
+          >
+            토너
+          </button>
         </li>
         <li>
-          <button className="Filter_Button">에센스</button>
+          <button
+            className="Filter_Button"
+            onClick={changeCategory}
+            name="에센스"
+          >
+            에센스
+          </button>
         </li>
         <li>
-          <button className="Filter_Button">로션</button>
+          <button
+            className="Filter_Button"
+            onClick={changeCategory}
+            name="로션"
+          >
+            로션
+          </button>
         </li>
         <li>
-          <button className="Filter_Button">크림</button>
+          <button
+            className="Filter_Button"
+            onClick={changeCategory}
+            name="크림"
+          >
+            크림
+          </button>
         </li>
       </ul>
       <div className="Products_Gallery">
