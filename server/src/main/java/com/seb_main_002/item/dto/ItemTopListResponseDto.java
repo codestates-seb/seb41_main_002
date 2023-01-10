a1899e30425e7f9658a91cdb76ee1d890b221b21
bum19 <1996joon@gmail.com>
2023-01-10 21:26:40 +0900

    [BE Refactor] TopList 반환 Dto코드 직관적이게 수정, 엔티티-DTO 매핑 방법 생성자이용->빌더패턴으로 변경

--- server/src/main/java/com/seb_main_002/item/dto/ItemTopListResponseDto.java
+++ server/src/main/java/com/seb_main_002/item/dto/ItemTopListResponseDto.java
@@ -28,15 +28,5 @@ public class ItemTopListResponseDto {
         private Integer salesCount;
         private List<String> tagsList;
 
-        public TopItemDto(Item item){
-            this.itemId = item.getItemId();
-            this.itemTitle = item.getItemTitle();
-            this.categoryKRName = item.getCategoryKRName();
-            this.categoryENName = item.getCategoryENName();
-            this.titleImageURL = item.getTitleImageUrl();
-            this.price = item.getPrice();
-            this.salesCount = item.getSalesCount();
-            this.tagsList = item.getTagList();
-        }
     }
 }
