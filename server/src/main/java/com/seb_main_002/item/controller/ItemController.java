67873c21146dcb51f4072dd85fa80c72950ad624
bum19 <1996joon@gmail.com>
2023-01-10 17:49:41 +0900

    [BE Feat] item 카테고리별 toplist 반환하는 로직 구현

--- server/src/main/java/com/seb_main_002/item/controller/ItemController.java
+++ server/src/main/java/com/seb_main_002/item/controller/ItemController.java
@@ -1,15 +1,19 @@
 package com.seb_main_002.item.controller;
 
 import com.seb_main_002.item.dto.ItemPostDto;
+import com.seb_main_002.item.dto.ItemTopListResponseDto;
 import com.seb_main_002.item.entity.Item;
 import com.seb_main_002.item.mapper.ItemMapper;
 import com.seb_main_002.item.service.ItemService;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/items")
@@ -41,4 +45,17 @@ public class ItemController {
 
         return new ResponseEntity<>(createdItem,HttpStatus.CREATED);
     }
+
+
+    @GetMapping("/toplist/{categoryENName}")
+    public ResponseEntity getItems(@PathVariable("categoryENName") String categoryENName){
+        List<Item> items = itemService.findTopListItems(categoryENName);
+        List<ItemTopListResponseDto> response = items.stream()
+                .map(item -> new ItemTopListResponseDto(item))
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(new ItemTopListResponseDto.Response(response),HttpStatus.OK);
+    }
+
+
 }
