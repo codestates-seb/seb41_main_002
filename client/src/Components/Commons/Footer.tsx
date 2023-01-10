e916acf7b4649ca1e391207727d73e1ac909ba77
rlawodn46465 <rlawodn46465@naver.com>
2023-01-10 23:49:04 +0900

    [FE MISC] 클래스명 네이밍

--- client/src/Components/Commons/Footer.tsx
+++ client/src/Components/Commons/Footer.tsx
@@ -19,35 +19,35 @@ const Footer = () => {
     <FooterContainer>
       <div className="Contents_Wrap">
         <div className="Footer_Inside">
-          <div className="left">
-            <div className="left_1">
+          <div className="Footer_Members">
+            <div className="Member_Name">
               <ul>
                 <li className="Footer_Title"><h3>팀원</h3></li>
-                <li className="Position">FrontEnd</li>
+                <li className="Footer_Position">FrontEnd</li>
                 <li>임경인</li>
                 <li>심민섭</li>
                 <li>김재우</li>
-                <li className="Position">BackEnd</li>
+                <li className="Footer_Position">BackEnd</li>
                 <li>주성천</li>
                 <li>이준범</li>
                 <li>김정우</li>
               </ul>
             </div>
-            <div className="left_2">
+            <div className="Member_GitHub">
               <ul>
                 <li className="Footer_Title"><h3>GitHub</h3></li>
-                <li className="Position">FrontEnd</li>
+                <li className="Footer_Position">FrontEnd</li>
                 <li><a href="https://github.com/limdumb">https://github.com/limdumb</a></li>
                 <li><a href="https://github.com/shim5505">https://github.com/shim5505</a></li>
                 <li><a href="https://github.com/rlawodn46465">https://github.com/rlawodn46465</a></li>
-                <li className="Position">BackEnd</li>
+                <li className="Footer_Position">BackEnd</li>
                 <li><a href="https://github.com/jution113">https://github.com/jution113</a></li>
                 <li><a href="https://github.com/bum19">https://github.com/bum19</a></li>
                 <li><a href="https://github.com/woojcoding">https://github.com/woojcoding</a></li>
               </ul>
             </div>
           </div>
-          <div className="right">
+          <div className="Project_Outline">
             <h3 className="Footer_Title">L’acier</h3>
             <p>프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명</p>
           </div>
