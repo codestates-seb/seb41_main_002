4cbb221976fa52352f0fb9d3c2d41dcfad354712
bum19 <1996joon@gmail.com>
2023-01-10 10:09:22 +0900

    [BE Feat] ItemPostDto 생성

---
+++ server/src/main/java/com/seb_main_002/item/dto/ItemPostDto.java
@@ -0,0 +1,38 @@
+package com.seb_main_002.item.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class ItemPostDto {
+
+    @NotNull
+    private String itemTitle;
+
+    @NotNull
+    List<String> tagList = new ArrayList<>();
+
+    @NotNull
+    private Integer price;
+
+    @NotNull
+    private String content;
+
+    @NotNull
+    private Double rating;
+
+    @NotNull
+    private Integer salesCount;
+
+    @NotNull
+    private String categoryKRName;
+
+    @NotNull
+    @Pattern(regexp = "^([A-Za-z])(\\s?[A-Za-z])*$", message = " categoryENName은 영문이어야 합니다.")
+    private String categoryENName;
+
+}
