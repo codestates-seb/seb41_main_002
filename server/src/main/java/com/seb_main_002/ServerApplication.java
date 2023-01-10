475611c551ef948e0a716b17edf2a6401ad31cb9
woojcoding <110895459+woojcoding@users.noreply.github.com>
2023-01-10 11:39:27 +0900

    [BE feat] @EnableJpaAuditing 어노테이션 추가

--- server/src/main/java/com/seb_main_002/ServerApplication.java
+++ server/src/main/java/com/seb_main_002/ServerApplication.java
@@ -2,8 +2,10 @@ package com.seb_main_002;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ServerApplication {
 
 	public static void main(String[] args) {
