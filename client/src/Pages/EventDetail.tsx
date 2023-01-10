9f69bfa1bc51d884414867e44cf209c5a8864bf4
minseob shim <shim5505@gmail.com>
2023-01-10 16:13:01 +0900

    [FE layout]세부 내용 추가 및 기본 CSS 구성

--- client/src/Pages/EventDetail.tsx
+++ client/src/Pages/EventDetail.tsx
@@ -3,21 +3,38 @@ import "./Style/eventDetail.css";
 export default function EventDetail() {
   return (
     <div>
-      <h1>이벤트 제목</h1>
-      <span>2023.01.10</span>
+      <h1 className="Event_Title">이벤트 제목</h1>
+      <div className="Event_Posted_Date">2023.01.10</div>
 
-      <div>
+      <div className="Event_Image">
         <img src="https://picsum.photos/1000/500" alt="sample image"></img>
       </div>
 
       <div>
-        <span>종료: 2023.02.03</span>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id repellat
-          sed quaerat exercitationem enim dolore ratione dolorem? Odio ab
-          temporibus excepturi, at neque, consequatur aliquam labore, mollitia
-          error perspiciatis vero!
-        </p>
+        <div className="Event_Term">
+          기간:
+          <span>2023.01.10</span> ~ <span>2023.02.03</span>
+        </div>
+        <div className="Event_Detail">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id
+            repellat sed quaerat exercitationem enim dolore ratione dolorem?
+            Odio ab temporibus excepturi, at neque, consequatur aliquam labore,
+            mollitia error perspiciatis vero! Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Id repellat sed quaerat exercitationem
+            enim dolore ratione dolorem? Odio ab temporibus excepturi, at neque,
+            consequatur aliquam labore, mollitia error perspiciatis vero!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id
+            repellat sed quaerat exercitationem enim dolore ratione dolorem?
+            Odio ab temporibus excepturi, at neque, consequatur aliquam labore,
+            mollitia error perspiciatis vero! Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Id repellat sed quaerat exercitationem
+            enim dolore ratione dolorem? Odio ab temporibus excepturi, at neque,
+            consequatur aliquam labore, mollitia error perspiciatis vero!
+          </p>
+        </div>
       </div>
     </div>
   );
