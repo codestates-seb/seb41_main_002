e46fb886c16202fa0a308cdc61895dd1515cae0f
woojcoding <110895459+woojcoding@users.noreply.github.com>
2023-01-10 11:39:08 +0900

    [BE feat] 구독 취소 및 신청시 Subscribe 필드값들 변경 로직 추가

--- server/src/main/java/com/seb_main_002/member/service/MemberService.java
+++ server/src/main/java/com/seb_main_002/member/service/MemberService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Transactional(readOnly = true)
@@ -23,7 +24,20 @@ public class MemberService {
         Subscribe subscribe = member.getSubscribe();
 
         if(subscribe.getIsSubscribed() !=isSubScribed) {
-            subscribe.setIsSubscribed(isSubScribed);
+            //구독 취소할 경우
+            if (subscribe.getIsSubscribed() == true) {
+                subscribe.setIsSubscribed(isSubScribed);
+                subscribe.setSampleCount(0);
+                subscribe.setReserveProfit(0);
+                subscribe.setTotalDeliveryDiscount(0);
+                subscribe.setSubScribedDate(null);
+            } else {
+                //구독 신청의 경우
+                subscribe.setIsSubscribed(isSubScribed);
+                Integer sampleCount = subscribe.getSampleCount();
+                subscribe.setSampleCount(sampleCount + 10);
+                subscribe.setSubScribedDate(LocalDateTime.now());
+            }
             member.setSubscribe(subscribe);
             memberRepository.save(member);
         } else {
