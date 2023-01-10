4f4befe2d49650339534061bf68b4c689326fa39
rlawodn46465 <rlawodn46465@naver.com>
2023-01-10 23:36:46 +0900

    [FE Design] 푸터 디자인

--- client/src/Components/Commons/Footer.tsx
+++ client/src/Components/Commons/Footer.tsx
@@ -1,17 +1,57 @@
-import React from "react";
 import styled from "styled-components";
+import "../Style/footer.css";
 
 const FooterContainer = styled.div`
   width: 100%;
-  height: 300px;
-  background-color: red;
+  padding: 20px 0;
+  border-top: 1px solid var(--lightgray);
+  background-color: var(--dark);
+
+  .Contents_Wrap {
+    background-color: var(--dark);
+  }
+
+  
 `;
 
 const Footer = () => {
   return (
     <FooterContainer>
       <div className="Contents_Wrap">
-        
+        <div className="Footer_Inside">
+          <div className="left">
+            <div className="left_1">
+              <ul>
+                <li className="Footer_Title"><h3>팀원</h3></li>
+                <li className="Position">FrontEnd</li>
+                <li>임경인</li>
+                <li>심민섭</li>
+                <li>김재우</li>
+                <li className="Position">BackEnd</li>
+                <li>주성천</li>
+                <li>이준범</li>
+                <li>김정우</li>
+              </ul>
+            </div>
+            <div className="left_2">
+              <ul>
+                <li className="Footer_Title"><h3>GitHub</h3></li>
+                <li className="Position">FrontEnd</li>
+                <li><a href="https://github.com/limdumb">https://github.com/limdumb</a></li>
+                <li><a href="https://github.com/shim5505">https://github.com/shim5505</a></li>
+                <li><a href="https://github.com/rlawodn46465">https://github.com/rlawodn46465</a></li>
+                <li className="Position">BackEnd</li>
+                <li><a href="https://github.com/jution113">https://github.com/jution113</a></li>
+                <li><a href="https://github.com/bum19">https://github.com/bum19</a></li>
+                <li><a href="https://github.com/woojcoding">https://github.com/woojcoding</a></li>
+              </ul>
+            </div>
+          </div>
+          <div className="right">
+            <h3 className="Footer_Title">L’acier</h3>
+            <p>프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명,프로젝트 설명</p>
+          </div>
+        </div>
       </div>
     </FooterContainer>
   );
