ad759c26c28a2c74493bc14f3576229351d6fbe5
rlawodn46465 <rlawodn46465@naver.com>
2023-01-11 10:25:43 +0900

    [FE Design] 모달 닫기 버튼 디자인

--- client/src/Components/Commons/Modal.tsx
+++ client/src/Components/Commons/Modal.tsx
@@ -14,15 +14,40 @@ const ModalOverlay = styled.div`
 const ModalContainer = styled.div`
   width: 400px;
   height: 500px;
-  position: absolute;
+  position: relative;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   background-color: var(--dark3);
   border-radius: 10px;
 
-  .Close_btn{
-    color: var(--gray);
+  .Close_Btn{
+    position: absolute;
+    top: 13px;
+    right: 13px;
+    width: 32px;
+    height: 32px;
+    cursor: pointer;
+  }
+
+  .Close_Btn > span{
+    position: absolute;
+    top: 50%;
+    width: 100%;
+    height: 3px;
+    background-color: var(--gray);
+  }
+
+  .Close_Btn:hover > span{
+    background-color: var(--white);
+  }
+
+  .Close_Btn > span:first-child{
+    transform: rotate(135deg) translateX(0%);
+  }
+  
+  .Close_Btn > span:last-child{
+    transform: rotate(45deg) translateX(0%);
   }
 `;
 
@@ -30,8 +55,9 @@ const Modal = () => {
   return (
     <ModalOverlay>
       <ModalContainer>
-        <div className="Close_btn">
-
+        <div className="Close_Btn">
+          <span></span>
+          <span></span>
         </div>
       </ModalContainer>
     </ModalOverlay>
