054dc29a819663cae4bb48ce8236ff701e7db49c
SungCheon Ju <jution113@gmail.com>
2023-01-11 17:53:28 +0900

    [BE Fix] git commit 충돌 해결

---
+++ server/src/main/java/com/seb_main_002/item/dto/ItemTopListResponseDto.java
@@ -0,0 +1,32 @@
+package com.seb_main_002.item.dto;
+
+import com.seb_main_002.item.entity.Item;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Builder
+@Getter
+public class ItemTopListResponseDto {
+
+    List<TopItemDto> topList;
+
+    public ItemTopListResponseDto(List<TopItemDto> topList){
+        this.topList = topList;
+    }
+
+    @Builder
+    @Getter
+    public static class TopItemDto{
+        private Long itemId;
+        private String itemTitle;
+        private String categoryKRName;
+        private String categoryENName;
+        private String titleImageURL;
+        private Integer price;
+        private Integer salesCount;
+        private List<String> tagsList;
+
+    }
+}
