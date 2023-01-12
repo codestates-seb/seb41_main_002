8c2cfd7dca1d5db7276c161bd8fb703aebf65984
minseob shim <shim5505@gmail.com>
2023-01-12 21:14:37 +0900

    [FE Feat] 홈 화면 Carousel 기능 구현

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -2,6 +2,7 @@ import "./Style/home.css";
 import dummyData from "./../data/HomeData.json";
 import styled from "styled-components";
 import CustomButton from "../Components/Commons/Buttons";
+import React, { useEffect, useState } from "react";
 
 const HeroImage = styled.div<{ bgUrl: string }>`
   background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
@@ -33,8 +34,43 @@ const BannerContent = styled.img<{ width: string }>`
   height: 100%;
 `;
 
+const CarouselSlide = styled.div<{ bgUrl: string }>`
+  position: relative;
+  flex-basis: 100%;
+  flex-shrink: 0;
+  background-image: url(${(props) => props.bgUrl});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  transition: all 1s;
+`;
+
 export default function Home() {
+  const [xPos, setXPos] = useState(0);
+  const [carouselStyleToSlide, setCarouselStyleToSlide] = useState({
+    transform: `translateX(${xPos}px)`,
+  });
   // console.log(dummyData);
+
+  useEffect(() => {
+    setCarouselStyleToSlide({ transform: `translateX(${xPos}px)` });
+  }, [xPos]);
+
+  const slideCarousel = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const arrowButton: HTMLButtonElement = event.currentTarget;
+    const maxXPos: number = -800 * (dummyData.eventsInfo.length - 1);
+
+    if (arrowButton.name === "left" && xPos === 0) {
+      setXPos(maxXPos);
+    } else if (arrowButton.name === "left" && xPos !== 0) {
+      setXPos(xPos + 800);
+    } else if (arrowButton.name === "right" && xPos === maxXPos) {
+      setXPos(0);
+    } else if (arrowButton.name === "right" && xPos > maxXPos) {
+      setXPos(xPos - 800);
+    }
+  };
+
   return (
     <div className="Home_Container">
       <HeroImage bgUrl={dummyData.bannerImageUrl}>
@@ -49,20 +85,28 @@ export default function Home() {
         </div>
       </HeroImage>
       <div className="Events_Banner">
-        <button className="left"> ◀ </button>
+        <button className="Carousel_Arrow" name="left" onClick={slideCarousel}>
+          ◀
+        </button>
         <div className="Events_Carousel">
           {dummyData.eventsInfo.map((a) => {
             return (
-              <div className="Event_Slide fade">
-                <div className="Carousel_Page">
+              <CarouselSlide
+                bgUrl={a.eventImageURL}
+                key={`slide${a.eventId}`}
+                style={carouselStyleToSlide}
+              >
+                <div className="Event_Number_Text">
                   {a.eventId} / {dummyData.eventsInfo.length}
                 </div>
                 <div className="Event_Caption_Text">{a.title}</div>
-              </div>
+              </CarouselSlide>
             );
           })}
         </div>
-        <button className="right"> ▶ </button>
+        <button className="Carousel_Arrow" name="right" onClick={slideCarousel}>
+          ▶
+        </button>
       </div>
       <TopBannerContainer height="250px">
         <BannerContent src="" width={"250px"} />
