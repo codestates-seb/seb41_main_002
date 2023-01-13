bf57bb7c13c982c0a0eb9792ce9b2aff5888e7f5
bum19 <1996joon@gmail.com>
2023-01-13 17:14:52 +0900

    [BE MISC] .

--- server/src/main/java/com/seb_main_002/eventImage/entity/Event.java
+++ server/src/main/java/com/seb_main_002/eventImage/entity/Event.java
@@ -40,4 +40,5 @@ public class Event extends Auditable {
         }
     }
 
+
 }
--- server/src/main/java/com/seb_main_002/eventImage/repository/EventRepository.java
+++ server/src/main/java/com/seb_main_002/eventImage/repository/EventRepository.java
@@ -4,4 +4,5 @@ import com.seb_main_002.eventImage.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
+
 }
