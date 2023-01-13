2cbffb5f96ad902232cd5e0ced1beb9abcdcf5aa
SungCheon Ju <jution113@gmail.com>
2023-01-13 10:37:35 +0900

    [BE Feat] Implement AddressMapper default mapper

--- server/src/main/java/com/seb_main_002/address/mapper/AddressMapper.java
+++ server/src/main/java/com/seb_main_002/address/mapper/AddressMapper.java
@@ -2,9 +2,22 @@ package com.seb_main_002.address.mapper;
 
 import com.seb_main_002.address.dto.AddressPostDto;
 import com.seb_main_002.address.entity.Address;
+import com.seb_main_002.member.entity.Member;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface AddressMapper {
-    Address addressPostDtoToAddress(AddressPostDto addressPostDto);
+    default Address addressPostDtoToAddress(AddressPostDto addressPostDto) {
+        Address address = new Address();
+        Member member = new Member();
+        member.setMemberId(addressPostDto.getMemberId());
+
+        address.setAddressTitle(addressPostDto.getAddressTitle());
+        address.setAddress(addressPostDto.getAddress());
+        address.setIsPrimary(addressPostDto.getIsPrimary());
+        address.setZipcode(addressPostDto.getZipcode());
+        address.setMember(member);
+
+        return address;
+    }
 }
