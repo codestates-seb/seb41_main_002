a5f9e2a7d344216854447aa0dd052397cbfe93f2
bum19 <1996joon@gmail.com>
2023-01-13 17:22:16 +0900

    [BE MISC] ItemRepository 쿼리문 변경

--- server/src/main/java/com/seb_main_002/item/repository/ItemRepository.java
+++ server/src/main/java/com/seb_main_002/item/repository/ItemRepository.java
@@ -15,10 +15,10 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     @Query(value = "SELECT ITEM_ID, CREATED_AT, LAST_MODIFIED_AT, CATEGORYENNAME, CATEGORYKRNAME, CONTENT, CONTENT_IMAGE_URL, ITEM_TITLE, PRICE, SALES_COUNT, RATING,TITLE_IMAGE_URL\n" +
             "FROM ITEM LEFT JOIN ITEM_TAG_LIST ON ITEM.ITEM_ID =ITEM_TAG_LIST.ITEM_ITEM_ID\n" +
-            "WHERE TAG_LIST = '건성' \n" +
-            "OR TAG_LIST = '일반피부'\n" +
-            "AND CATEGORYENNAME LIKE 'cream%'\n" +
-            "AND ITEM_TITLE LIKE '%%'\n" +
+            "WHERE TAG_LIST = :tag1 \n" +
+            "OR TAG_LIST = :tag2\n" +
+            "AND CATEGORYENNAME LIKE :categoryENName% \n" +
+            "AND ITEM_TITLE LIKE %:title% \n" +
             "GROUP BY ITEM_ID\n" +
             "HAVING COUNT(ITEM_ID) >1", nativeQuery = true)
     Page<Item> findByCustomItem(String tag1, String tag2, String categoryENName, String title, Pageable pageable);
