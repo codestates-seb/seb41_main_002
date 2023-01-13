c90d804e4f966a1a7aa1042f665983482fa1c9a3
bum19 <1996joon@gmail.com>
2023-01-13 13:55:24 +0900

    [BE Feat] ItemRepository.java/ native 쿼리문을 이용해 상품 리스트 조회 메서드 구현

--- server/src/main/java/com/seb_main_002/item/repository/ItemRepository.java
+++ server/src/main/java/com/seb_main_002/item/repository/ItemRepository.java
@@ -4,7 +4,25 @@ import com.seb_main_002.item.entity.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
     Page<Item> findAllByCategoryENName(String categoryENName, Pageable pageable);
+
+    @Query(value = "SELECT ITEM_ID, CREATED_AT, LAST_MODIFIED_AT, CATEGORYENNAME, CATEGORYKRNAME, CONTENT, CONTENT_IMAGE_URL, ITEM_TITLE, PRICE, SALES_COUNT, RATING,TITLE_IMAGE_URL\n" +
+            "FROM ITEM LEFT JOIN ITEM_TAG_LIST ON ITEM.ITEM_ID =ITEM_TAG_LIST.ITEM_ITEM_ID\n" +
+            "WHERE TAG_LIST = '건성' \n" +
+            "OR TAG_LIST = '일반피부'\n" +
+            "AND CATEGORYENNAME LIKE 'cream%'\n" +
+            "AND ITEM_TITLE LIKE '%%'\n" +
+            "GROUP BY ITEM_ID\n" +
+            "HAVING COUNT(ITEM_ID) >1", nativeQuery = true)
+    Page<Item> findByCustomItem(String tag1, String tag2, String categoryENName, String title, Pageable pageable);
+
+    Page<Item> findAllByCategoryENNameContainingAndItemTitleContaining(String categoryENName, String title, Pageable pageable);
+
 }
