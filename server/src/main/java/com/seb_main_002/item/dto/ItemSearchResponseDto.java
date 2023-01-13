13873042162a208028fe93c42a3b8b510c0b548b
bum19 <1996joon@gmail.com>
2023-01-13 13:53:54 +0900

    [BE Feat] 상품리스트 페이지용 Dto 구현

--- server/src/main/java/com/seb_main_002/item/dto/ItemSearchResponseDto.java
+++ server/src/main/java/com/seb_main_002/item/dto/ItemSearchResponseDto.java
@@ -1,4 +1,25 @@
 package com.seb_main_002.item.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Builder
+@Getter
 public class ItemSearchResponseDto {
+    List<SearchItemDto> cosmetics;
+
+    @Builder
+    @Getter
+    public static class SearchItemDto{
+        private Long itemId;
+        private String itemTitle;
+        private String categoryKRName;
+        private String categoryENName;
+        private String titleImageURL;
+        private Integer price;
+        private List<String> tagsList;
+    }
+
 }
