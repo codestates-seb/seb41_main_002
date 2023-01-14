55ad86fb7b8483212b9c4658d43980666ebd5510
SungCheon Ju <jution113@gmail.com>
2023-01-14 12:24:17 +0900

    [BE Feat] AddressRepository에 memberId를 기준으로 address를 찾는 메소드 구현

--- server/src/main/java/com/seb_main_002/address/repository/AddressRepository.java
+++ server/src/main/java/com/seb_main_002/address/repository/AddressRepository.java
@@ -2,8 +2,13 @@ package com.seb_main_002.address.repository;
 
 import com.seb_main_002.address.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+    @Query(value = "SELECT * FROM ADDRESS WHERE MEMBER_ID = :memberId", nativeQuery = true)
+    List<Address> findAddressesByMemberId(Long memberId);
 }
