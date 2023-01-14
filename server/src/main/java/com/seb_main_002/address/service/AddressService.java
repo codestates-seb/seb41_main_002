a78f7b95782df9a85e5659b33d07216bcb19614d
SungCheon Ju <jution113@gmail.com>
2023-01-14 12:25:23 +0900

    [BE Refactor] AddressSerivce에 주소 중복 검사 로직 수정

--- server/src/main/java/com/seb_main_002/address/service/AddressService.java
+++ server/src/main/java/com/seb_main_002/address/service/AddressService.java
@@ -4,7 +4,11 @@ import com.seb_main_002.address.entity.Address;
 import com.seb_main_002.address.repository.AddressRepository;
 import com.seb_main_002.exception.BusinessLogicException;
 import com.seb_main_002.exception.ExceptionCode;
+import com.seb_main_002.member.entity.Member;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AddressService {
@@ -15,35 +19,37 @@ public class AddressService {
     }
 
     public void createAddress(Address address) {
-        verifyDuplicateAddress(address);
+        verifyDuplicateAddress(address, "post");
 
         if(address.getIsPrimary()) {
-            addressRepository.findAll().forEach(existsAddress -> {
-                existsAddress.setIsPrimary(false);
-            });
+            changePrimaryAddress(address.getMember().getMemberId());
         }
 
         addressRepository.save(address);
     }
 
-    private void changePrimaryAddress(Long memberId) {
-            addressRepository.findAll().forEach(addressInfo -> {
-                if(addressInfo.getMember().getMemberId().equals(memberId)) {
-                    addressInfo.setIsPrimary(false);
-                }
-        });
+    public void updateAddress(Address address) {
+        Address verifiedAddress = verifyExistAddress(address.getAddressId());
+
+        address.setMember(new Member());
+        address.getMember().setMemberId(verifiedAddress.getMember().getMemberId());
+        verifyDuplicateAddress(address, "patch");
+
+        if(address.getIsPrimary()) {
+            changePrimaryAddress(verifiedAddress.getMember().getMemberId());
+        }
+
+        Optional.ofNullable(address.getIsPrimary()).ifPresent(isPrimary -> verifiedAddress.setIsPrimary(isPrimary));
+        Optional.ofNullable(address.getAddressTitle()).ifPresent(addressTitle -> verifiedAddress.setAddressTitle(addressTitle));
+        Optional.ofNullable(address.getZipcode()).ifPresent(zipcode -> verifiedAddress.setZipcode(zipcode));
+        Optional.ofNullable(address.getAddress()).ifPresent(addressContent -> verifiedAddress.setAddress(addressContent));
+
+        addressRepository.save(verifiedAddress);
     }
 
-    private void verifyDuplicateAddress(Address address) {
-        Long memberId = address.getMember().getMemberId();
-
-        addressRepository.findAll().forEach(addressInfo -> {
-            if(addressInfo.getMember().getMemberId().equals(memberId)) {
-                boolean addressCheck = addressInfo.getAddress().equals(address.getAddress());
-                boolean zipcodeCheck = addressInfo.getZipcode().equals(address.getZipcode());
-
-                if(addressCheck && zipcodeCheck) throw new BusinessLogicException(ExceptionCode.ADDRESS_EXISTS);
-            }
+    private void changePrimaryAddress(Long memberId) {
+        addressRepository.findAddressesByMemberId(memberId).forEach(memberAddress -> {
+            memberAddress.setIsPrimary(false);
         });
     }
 
@@ -52,15 +58,20 @@ public class AddressService {
         return optionalAddress.orElseThrow(() -> new BusinessLogicException(ExceptionCode.ADDRESS_NOT_FOUND));
     }
 
-    private void verifyDuplicateAddress(Address address) {
+    private void verifyDuplicateAddress(Address address, String requestMethodType) {
+        boolean isNewRegister = requestMethodType.equals("post");
+
         Long memberId = address.getMember().getMemberId();
+        List<Address> memberAddressList = addressRepository.findAddressesByMemberId(memberId);
 
-        addressRepository.findAll().forEach(addressInfo -> {
-            if(addressInfo.getMember().getMemberId().equals(memberId)) {
-                boolean addressCheck = addressInfo.getAddress().equals(address.getAddress());
-                boolean zipcodeCheck = addressInfo.getZipcode().equals(address.getZipcode());
+        memberAddressList.forEach(memberAddress -> {
+            if(!isNewRegister && address.getAddressId().equals(memberAddress.getAddressId())) {
+                // 신규 등록이 아닐 경우 수정하려는 주소의 자신(해당 addressId)에 해당하는 주소는 중복 검사를 실행하지 않음
+            } else {
+                boolean addressCheck = memberAddress.getAddress().equals(address.getAddress());
+                boolean zipcodeCheck = memberAddress.getZipcode().equals(address.getZipcode());
 
-                if(addressCheck && zipcodeCheck) throw new BusinessLogicException(ExceptionCode.ADDRESS_EXISTS);
+                if (addressCheck && zipcodeCheck) throw new BusinessLogicException(ExceptionCode.ADDRESS_EXISTS);
             }
         });
     }
