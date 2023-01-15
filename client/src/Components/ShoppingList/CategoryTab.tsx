363a137aa13b53520d68f23cb3bd24857ece098b
limdumb <dlaruddls@nate.com>
2023-01-15 23:23:42 +0900

    [FE FIX] key오류 해결

--- client/src/Components/ShoppingList/CategoryTab.tsx
+++ client/src/Components/ShoppingList/CategoryTab.tsx
@@ -1,36 +1,42 @@
 import CustomButton from "../Commons/Buttons";
 import "./Style/categoryTab.css";
 
-interface Category{
-  categoryENName: string
-  categoryKRName: string
+interface Category {
+  categoryENName: string;
+  categoryKRName: string;
 }
 
 export const ShoppingCategoryTab: Function = () => {
-  const categoryTitle: Array<Category>= [{
-    categoryKRName: "전체",
-    categoryENName: "all"
-  },{
-    categoryKRName: "토너",
-    categoryENName: "toner"
-  },{
-    categoryKRName: "크림",
-    categoryENName: "cream"
-  },{
-    categoryKRName: "로션",
-    categoryENName: "lotion"
-  },{
-    categoryKRName: "클렌징",
-    categoryENName: "cleansing"
-  },{
-    categoryKRName: "선크림",
-    categoryENName: "suncream"
-  }
-];
+  const categoryTitle: Array<Category> = [
+    {
+      categoryKRName: "전체",
+      categoryENName: "all",
+    },
+    {
+      categoryKRName: "토너",
+      categoryENName: "toner",
+    },
+    {
+      categoryKRName: "크림",
+      categoryENName: "cream",
+    },
+    {
+      categoryKRName: "로션",
+      categoryENName: "lotion",
+    },
+    {
+      categoryKRName: "클렌징",
+      categoryENName: "cleansing",
+    },
+    {
+      categoryKRName: "선크림",
+      categoryENName: "suncream",
+    },
+  ];
 
   return categoryTitle.map((category) => {
     return (
-      <li  onClick={()=> category.categoryENName}>
+      <li key={category.categoryENName} onClick={() => category.categoryENName}>
         <CustomButton
           fontColor="white"
           bgColor="gray"
