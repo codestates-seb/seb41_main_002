3a8a8edba532fb47f6ffc5a1cb0f0c4a0796cc7d
SungCheon Ju <jution113@gmail.com>
2023-01-16 21:35:49 +0900

    [BE Feat] Implement ReviewService에 review-item get api

--- server/src/main/java/com/seb_main_002/review/service/ReviewService.java
+++ server/src/main/java/com/seb_main_002/review/service/ReviewService.java
@@ -2,6 +2,11 @@ package com.seb_main_002.review.service;
 
 import com.seb_main_002.exception.BusinessLogicException;
 import com.seb_main_002.exception.ExceptionCode;
+import com.seb_main_002.item.entity.Item;
+import com.seb_main_002.item.repository.ItemRepository;
+import com.seb_main_002.member.entity.Member;
+import com.seb_main_002.member.repository.MemberRepository;
+import com.seb_main_002.review.dto.ReviewResponseDto;
 import com.seb_main_002.review.entity.Review;
 import com.seb_main_002.review.repository.ReviewRepository;
 import org.springframework.stereotype.Service;
@@ -11,9 +16,15 @@ import java.util.Optional;
 @Service
 public class ReviewService {
     private final ReviewRepository reviewRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
 
-    public ReviewService(ReviewRepository reviewRepository) {
+    public ReviewService(ReviewRepository reviewRepository,
+                         MemberRepository memberRepository,
+                         ItemRepository itemRepository) {
         this.reviewRepository = reviewRepository;
+        this.memberRepository = memberRepository;
+        this.itemRepository = itemRepository;
     }
 
     public void createReview(Review review) {
@@ -40,6 +51,19 @@ public class ReviewService {
         return verifyExistsReview(reviewId);
     }
 
+    public ReviewResponseDto.ReviewItemDto findReviewItem(Long itemId, Long memberId) {
+        Item verifiedItem = verifyExistsItem(itemId);
+        Member verifiedMember = verifyExistsMember(memberId);
+
+        return ReviewResponseDto.ReviewItemDto.builder()
+                .itemTitle(verifiedItem.getItemTitle())
+                .categoryKRName(verifiedItem.getCategoryKRName())
+                .titleImageURL(verifiedItem.getTitleImageUrl())
+                .tagList(verifiedItem.getTagList())
+                .memberTagsList(verifiedMember.getTagList())
+                .build();
+    }
+
     public void deleteReview(Long reviewId) {
         verifyExistsReview(reviewId);
 
@@ -49,5 +73,15 @@ public class ReviewService {
     private Review verifyExistsReview(Long reviewId) {
         Optional<Review> optionalReview = reviewRepository.findById(reviewId);
         return optionalReview.orElseThrow(() -> new BusinessLogicException(ExceptionCode.REVIEW_NOT_FOUND));
+    }
+
+    private Item verifyExistsItem(Long itemId) {
+        Optional<Item> optionalItem = itemRepository.findById(itemId);
+        return optionalItem.orElseThrow(() -> new BusinessLogicException(ExceptionCode.ITEM_NOT_FOUND));
+    }
+
+    private Member verifyExistsMember(Long memberId) {
+        Optional<Member> optionalMember = memberRepository.findById(memberId);
+        return optionalMember.orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
     }
 }
