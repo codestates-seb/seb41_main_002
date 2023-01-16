6b1ca0c9a0b672daf5cebba4f1012a0594f4f4b7
SungCheon Ju <jution113@gmail.com>
2023-01-16 14:00:43 +0900

    [BE Feat] implement ReviewMapper

--- server/src/main/java/com/seb_main_002/review/mapper/ReviewMapper.java
+++ server/src/main/java/com/seb_main_002/review/mapper/ReviewMapper.java
@@ -1,2 +1,21 @@
-package com.seb_main_002.review.mapper;public interface ReviewMapper {
+package com.seb_main_002.review.mapper;
+
+import com.seb_main_002.member.entity.Member;
+import com.seb_main_002.review.dto.ReviewPatchDto;
+import com.seb_main_002.review.entity.Review;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ReviewMapper {
+    default Review reviewPatchDtoToReview(ReviewPatchDto reviewPatchDto) {
+        Review review = new Review();
+        Member member = new Member();
+        member.setMemberId(reviewPatchDto.getMemberId());
+
+        review.setMember(member);
+        review.setReviewTitle(reviewPatchDto.getReviewTitle());
+        review.setReviewContent(reviewPatchDto.getReviewContent());
+
+        return review;
+    }
 }
