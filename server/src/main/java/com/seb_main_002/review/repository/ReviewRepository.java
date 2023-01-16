9d7f03c81f952481ec9b906bff4085b9592305f6
SungCheon Ju <jution113@gmail.com>
2023-01-16 14:00:18 +0900

    [BE Feat] implement ReviewRepository

--- server/src/main/java/com/seb_main_002/review/repository/ReviewRepository.java
+++ server/src/main/java/com/seb_main_002/review/repository/ReviewRepository.java
@@ -1,2 +1,9 @@
-package com.seb_main_002.review.repository;public interface ReviewRepository {
+package com.seb_main_002.review.repository;
+
+import com.seb_main_002.review.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
