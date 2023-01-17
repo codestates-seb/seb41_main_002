a01f8bdbc594922ce9d9e34d06f6e705b86f70c4
minseob shim <shim5505@gmail.com>
2023-01-17 16:08:20 +0900

    [FE Feat] 기본 구성 완료 및 더미데이터 추가

--- client/src/Components/AddressPopup.tsx
+++ client/src/Components/AddressPopup.tsx
@@ -2,29 +2,54 @@ import { useState } from "react";
 import CustomButton from "./Commons/Buttons";
 
 export default function AddressPopup() {
-  const [isSearchPopupOpen, setIsSearchPopupOpen] = useState(false);
+  const [isSearchOn, setIsSearchOn] = useState(false);
 
   // 팝업창 열기
   const openPostCode = () => {
-    setIsSearchPopupOpen(true);
+    setIsSearchOn(true);
   };
 
   // 팝업창 닫기
   const closePostCode = () => {
-    setIsSearchPopupOpen(false);
+    setIsSearchOn(false);
   };
 
   return (
-    <div>
-      <div>도로명 입력</div>
-      <CustomButton
-        bgColor="white"
-        content="도로명 검색"
-        fontColor="black"
-        padding="5px"
-        width="150px"
-        onClick={openPostCode}
-      />
-    </div>
+    <form className="Address_Modal">
+      <h1 className="Address_Header"> 주소 추가 </h1>
+      <div className="Modal_Field">
+        <label htmlFor="">배송지명 </label>
+        <input type="text" className="Address_Input Address_Type_Field" />
+      </div>
+      <div className="Modal_Field">
+        <label htmlFor="">수령인 </label>
+        <input type="text" className="Address_Input Address_Type_Field" />
+      </div>
+      <div className="Modal_Field">
+        <label htmlFor="">주소 </label>
+        <input
+          type="text"
+          className="Address_Input Address_Search_Bar"
+          readOnly
+        />
+        <CustomButton
+          bgColor="white"
+          content="도로명 검색"
+          fontColor="black"
+          padding="10px"
+          width="125px"
+          onClick={openPostCode}
+        />
+      </div>
+      <div className="Modal_Field">
+        <label htmlFor="">상세 주소 </label>
+        <input type="text" className="Address_Input Address_Type_Field" />
+      </div>
+      <div className="Modal_Field">
+        <label htmlFor="">연락처 </label>
+        <input type="text" className="Address_Input Address_Type_Field" />
+      </div>
+      <input type="submit" className="Submit_Button" />
+    </form>
   );
 }
