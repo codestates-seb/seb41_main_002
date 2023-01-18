b25bbafbde8aa7e3c55bf2d49e33107fac027818
woojcoding <110895459+woojcoding@users.noreply.github.com>
2023-01-18 11:39:21 +0900

    [BE Remove] RedisTemplate를 이용하게 되어 필요 없는 파일 삭제

--- server/src/main/java/com/seb_main_002/security/redis/JwtRefreshTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.seb_main_002.security.redis;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JwtRefreshTokenRepository extends CrudRepository<JwtRefreshToken,String> {
-}
