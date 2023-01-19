af245a0bcf5ef54195e5186cade79fb3eba79cb9
limdumb <dlaruddls@nate.com>
2023-01-19 09:56:11 +0900

    [FE Feat] 장바구니 추가함수 구현

--- client/src/API/ItemDetail/addCartItem.ts
+++ client/src/API/ItemDetail/addCartItem.ts
@@ -1,6 +1,20 @@
-import axios from "axios"
+import axios from "axios";
 
-export default function addCartItem(){
-  
-  return
+interface ProductData {
+  memberId: number;
+  itemId: number;
+  itemCount: number;
+  itemTotalPrice: number;
 }
+
+export const addCartItem = (cartItem: ProductData) => {
+  const request = {
+    itemId: cartItem.itemId,
+    itemCount: cartItem.itemCount,
+    itemTotalPrice: cartItem.itemTotalPrice,
+  };
+  axios.post(
+    `http://13.209.97.3:8080/api/v1/members/${cartItem.memberId}/carts`,
+    JSON.stringify(request)
+  );
+};
