0b6c8738a971b9af21a64855942a0de6768a569b
SungCheon Ju <jution113@gmail.com>
2023-01-19 13:07:56 +0900

    [BE Refactor] OrderService와 OrderController 분리 작업

--- server/src/main/java/com/seb_main_002/order/controller/OrderController.java
+++ server/src/main/java/com/seb_main_002/order/controller/OrderController.java
@@ -3,9 +3,11 @@ package com.seb_main_002.order.controller;
 import com.seb_main_002.delivery.entity.Delivery;
 import com.seb_main_002.item.entity.Item;
 import com.seb_main_002.member.entity.Member;
+import com.seb_main_002.order.dto.OrderInfo;
 import com.seb_main_002.order.dto.OrderPostDto;
 import com.seb_main_002.order.entity.Order;
 import com.seb_main_002.order.entity.OrderItem;
+import com.seb_main_002.order.mapper.OrderMapper;
 import com.seb_main_002.order.service.OrderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,14 +22,25 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/v1/orders")
 public class OrderController {
+    private OrderMapper mapper;
     private OrderService orderService;
 
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderMapper mapper,
+                           OrderService orderService) {
+        this.mapper = mapper;
         this.orderService = orderService;
     }
 
     @PostMapping
     public ResponseEntity postOrder(@RequestBody OrderPostDto orderPostDto) {
+        Order order = mapper.orderPostDtoToOrder(orderPostDto);
+        OrderInfo orderInfo = mapper.orderPostDtoToOrderInfo(orderPostDto);
+
+        orderService.createOrder(order, orderInfo);
+
+
+
+        // ----- ----- ----- ----- -----
         Order order = new Order();
 
         // Order 기본 column 설정
@@ -62,6 +75,8 @@ public class OrderController {
         order.setDelivery(delivery);
 
         orderService.createOrder(order);
+
+        // ----- ----- ----- ----- -----
 
         return new ResponseEntity<> (HttpStatus.OK);
     }
--- server/src/main/java/com/seb_main_002/order/service/OrderService.java
+++ server/src/main/java/com/seb_main_002/order/service/OrderService.java
@@ -4,8 +4,10 @@ import com.seb_main_002.address.entity.Address;
 import com.seb_main_002.delivery.entity.Delivery;
 import com.seb_main_002.exception.BusinessLogicException;
 import com.seb_main_002.exception.ExceptionCode;
+import com.seb_main_002.item.repository.ItemRepository;
 import com.seb_main_002.member.entity.Member;
 import com.seb_main_002.member.repository.MemberRepository;
+import com.seb_main_002.order.dto.OrderInfo;
 import com.seb_main_002.order.entity.Order;
 import com.seb_main_002.order.repository.OrderRepository;
 import com.seb_main_002.subscribe.entity.Subscribe;
@@ -18,13 +20,54 @@ import java.util.Optional;
 public class OrderService {
     private OrderRepository orderRepository;
     private MemberRepository memberRepository;
+    private ItemRepository itemRepository;
 
-    public OrderService(OrderRepository orderRepository, MemberRepository memberRepository) {
+    public OrderService(OrderRepository orderRepository,
+                        MemberRepository memberRepository,
+                        ItemRepository itemRepository) {
         this.orderRepository = orderRepository;
         this.memberRepository = memberRepository;
+        this.itemRepository = itemRepository;
     }
 
-    public void createOrder(Order order) {
+    public void createOrder(Order order, OrderInfo orderInfo) {
+        // 1. 사용하려는 적립금이 보유한 적립금보다 많은지 확인
+        // 1.T. 사용하려는 적립금 >= 보유한 적립금 -> 2단계 이동
+        // 1.F. 사용하려는 적립금 < 보유한 적립금 -> 에러 발생
+        // 2. 적립금 차감 후 member에 저장
+        // 3. 구독 여부 확인 후 적립금 차등 적립
+        // 3.T. 구독중일 경우 5% 적립 후 member에 저장 -> 4단계 이동
+        // 3.F. 구독중이 아닐 경우 3% 적립 후 member에 저장 -> 4단계 이동
+        // 4. 구독 여부 확인 후 혜택 기록
+        // 4.T 구독중일 경우 배송비 2000원 혜택 + 2% 추가 적립 내역 subscribe에 기록 -> 5단계 이동
+        // 4.T 구독중이 아닐 경우 -> 5단계 이동
+        // 5. 대표 배송지 체크 여부에 따라 배송지 설정
+        // 5.T. 대표 배송지가 설정되어 있을 경우 배송지 정보를 delivery에 저장 후 member의 address 대표 배송지 상태 변경
+        // 5.F. 대표 배송지가 설정되어 있지 않을 경우 배송지 정보를 delivery에 저장
+
+        Long memberId = order.getMember().getMemberId();
+
+        Member verifiedMember = verifyMember(memberId);
+        boolean isSubscribed = verifiedMember.getSubscribe().getIsSubscribed();
+
+        // 사용하려는 적립금과 보유한 적립금을 계산 후 적립금 차감
+        if(verifiedMember.getMemberReserve() < orderInfo.getUsedReserve()) throw new BusinessLogicException(ExceptionCode.);
+        verifiedMember.setMemberReserve(verifiedMember.getMemberReserve() - orderInfo.getUsedReserve());
+
+        if(isSubscribed) {
+            verifiedMember.setMemberReserve(verifiedMember.getMemberReserve() + (orderInfo.getItemsTotalPrice() / 100) * 5);
+            verifiedMember.getSubscribe().setReserveProfit((orderInfo.getItemsTotalPrice() / 100) * 2);
+            verifiedMember.getSubscribe().setTotalDeliveryDiscount(verifiedMember.getSubscribe().getTotalDeliveryDiscount() + 2000);
+        } else {
+            verifiedMember.setMemberReserve(verifiedMember.getMemberReserve() + (orderInfo.getItemsTotalPrice() / 100) * 3);
+        }
+
+
+        // 수정된 member 정보 저장
+        memberRepository.save(verifiedMember);
+
+
+
         orderRepository.save(order);
     }
 
