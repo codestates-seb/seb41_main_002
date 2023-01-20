c84ee9917c53d797fde9916d5446e1cd70f6aa04
rlawodn46465 <rlawodn46465@naver.com>
2023-01-20 14:03:49 +0900

    [FE Feat] 리덕스 툴킷 세팅

---
+++ client/src/Store/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import tokenSlice from "./slices/token"
+
+export const store = configureStore({
+  reducer: {
+    token: tokenSlice
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
