ae08e586a2d1ad54731d38bb1eb81d207d1c69e9
woojcoding <110895459+woojcoding@users.noreply.github.com>
2023-01-27 11:09:41 +0900

    [BE Feat] 기존과 동일한 비밀번호로 요청하는 경우의 ExceptionCode 추가

--- server/src/main/java/com/seb_main_002/exception/ExceptionCode.java
+++ server/src/main/java/com/seb_main_002/exception/ExceptionCode.java
@@ -20,7 +20,8 @@ public enum ExceptionCode {
     REVIEW_NOT_FOUND(404,"Review not found"),
     CANNOT_MODIFY_REVIEW(403,"No permission to modify Review"),
     CART_ITEM_NOT_FOUND(404,"CartItem not found"),
-    EMAIL_NOT_FOUND(404, "Email not found");
+    EMAIL_NOT_FOUND(404, "Email not found"),
+    SAME_PASSWORD(409, "Password is same as before");
 
     @Getter
     private int status;
