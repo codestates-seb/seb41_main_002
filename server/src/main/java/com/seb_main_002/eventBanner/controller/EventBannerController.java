ca912ec2dd422e50fcc4a35d874158713b337db4
SungCheon Ju <jution113@gmail.com>
2023-01-27 13:09:23 +0900

    [BE Feat] EventController 구현

--- server/src/main/java/com/seb_main_002/eventBanner/controller/EventBannerController.java
+++ server/src/main/java/com/seb_main_002/eventBanner/controller/EventBannerController.java
@@ -1,50 +1,70 @@
-package com.seb_main_002.eventImage.controller;
+package com.seb_main_002.eventBanner.controller;
 
-import com.seb_main_002.eventImage.dto.EventPostDto;
-import com.seb_main_002.eventImage.entity.Banner;
-import com.seb_main_002.eventImage.entity.Event;
-import com.seb_main_002.eventImage.mapper.EventMapper;
-import com.seb_main_002.eventImage.service.EventImageService;
+import com.seb_main_002.eventBanner.dto.EventPostDto;
+
+import com.seb_main_002.eventBanner.entity.Banner;
+import com.seb_main_002.eventBanner.entity.Event;
+import com.seb_main_002.eventBanner.mapper.BannerMapper;
+import com.seb_main_002.eventBanner.mapper.EventMapper;
+import com.seb_main_002.eventBanner.service.EventBannerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/v1/image")
-public class EventImageController_D {
-    private final EventImageService eventImageService;
-    private final EventMapper mapper;
+@RequestMapping("/api/v1")
+@Validated
+public class EventBannerController {
+    private final EventBannerService eventBannerService;
+    private final EventMapper eventMapper;
+    private final BannerMapper bannerMapper;
 
-    EventImageController_D(EventImageService eventImageService, EventMapper mapper) {
-        this.eventImageService = eventImageService;
-        this.mapper = mapper;
+    EventBannerController(EventBannerService eventBannerService,
+                          EventMapper eventMapper,
+                          BannerMapper bannerMapper) {
+        this.eventBannerService = eventBannerService;
+        this.eventMapper = eventMapper;
+        this.bannerMapper = bannerMapper;
     }
 
     @PostMapping("/banner")
     public ResponseEntity postBannerImage(@RequestPart(value = "bannerImage") MultipartFile bannerImage) throws IOException {
         Banner banner = new Banner();
-        banner.setBannerImageUrl(eventImageService.uploadImage(bannerImage.getInputStream(),
+
+        banner.setBannerImageUrl(eventBannerService.uploadImage(bannerImage.getInputStream(),
                 bannerImage.getOriginalFilename(),
                 bannerImage.getSize()));
-        eventImageService.createBanner(banner);
 
-        return new ResponseEntity<>(banner, HttpStatus.OK);
+        Banner response = eventBannerService.createBanner(banner);
+
+        return new ResponseEntity<>(bannerMapper.bannerToBannerResponseDto(response), HttpStatus.OK);
     }
 
     @PostMapping("/event")
-    public ResponseEntity postEventImage(@RequestPart(value = "eventPostDto") EventPostDto eventPostDto,
-                                         @RequestPart(value = "eventImage") MultipartFile eventImage) throws IOException {
-        Event event = mapper.eventPostDtoToEvent(eventPostDto);
+    public ResponseEntity postEventImage(@RequestPart(value = "eventImage") MultipartFile eventImage,
+                                         @RequestPart(value = "eventPostDto") @Valid EventPostDto eventPostDto) throws IOException {
+        Event event = eventMapper.eventPostDtoToEvent(eventPostDto);
 
-        event.setEventImageUrl(eventImageService.uploadImage(eventImage.getInputStream(),
+        event.setEventImageUrl(eventBannerService.uploadImage(eventImage.getInputStream(),
                 eventImage.getOriginalFilename(),
                 eventImage.getSize()));
 
-        return new ResponseEntity<>(eventImageService.createEvent(event), HttpStatus.OK);
+        Event response = eventBannerService.createEvent(event);
+
+        return new ResponseEntity<>(eventMapper.eventToEventResponseDto(response), HttpStatus.OK);
     }
 
-    // todo get event 추가할지도?
+    @GetMapping("event/{eventId}")
+    public ResponseEntity getEvent(@PathVariable Long eventId){
+
+        Event response = eventBannerService.findEvent(eventId);
+
+        return new ResponseEntity(eventMapper.eventToEventResponseDto(response), HttpStatus.OK);
+    }
+
 }
