16ccee4fc9c0a863efc733f0c7892840f489b61d
SungCheon Ju <jution113@gmail.com>
2023-01-27 13:14:40 +0900

    [BE Feat] service 수정

--- server/src/main/java/com/seb_main_002/eventBanner/service/EventBannerService.java
+++ server/src/main/java/com/seb_main_002/eventBanner/service/EventBannerService.java
@@ -1,22 +1,25 @@
-package com.seb_main_002.eventImage.service;
+package com.seb_main_002.eventBanner.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.seb_main_002.eventImage.entity.Banner;
-import com.seb_main_002.eventImage.entity.Event;
-import com.seb_main_002.eventImage.repository.BannerRepository;
-import com.seb_main_002.eventImage.repository.EventRepository;
+import com.seb_main_002.eventBanner.entity.Banner;
+import com.seb_main_002.eventBanner.entity.Event;
+import com.seb_main_002.eventBanner.repository.BannerRepository;
+import com.seb_main_002.eventBanner.repository.EventRepository;
+import com.seb_main_002.exception.BusinessLogicException;
+import com.seb_main_002.exception.ExceptionCode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @Transactional
-public class EventImageService {
+public class EventBannerService {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
@@ -24,18 +27,29 @@ public class EventImageService {
     private final BannerRepository bannerRepository;
     private final EventRepository eventRepository;
 
-    public EventImageService(AmazonS3 amazonS3, BannerRepository bannerRepository, EventRepository eventRepository) {
+    public EventBannerService(AmazonS3 amazonS3, BannerRepository bannerRepository, EventRepository eventRepository) {
         this.amazonS3 = amazonS3;
         this.bannerRepository = bannerRepository;
         this.eventRepository = eventRepository;
+    }
+
+    public Banner createBanner(Banner banner){
+        return bannerRepository.save(banner);
     }
 
     public Event createEvent(Event event){
         return eventRepository.save(event);
     }
 
-    public Banner createBanner(Banner banner){
-        return bannerRepository.save(banner);
+    public Event findEvent(long eventId){
+        return findVerifiedEvent(eventId);
+    }
+
+    public Event findVerifiedEvent(long eventId){
+        Optional<Event> optionalEvent = eventRepository.findById(eventId);
+
+        return optionalEvent.orElseThrow(()->
+                new BusinessLogicException(ExceptionCode.EVENT_NOT_FOUND));
     }
 
     public String uploadImage(InputStream input, String fileName, long fileSize) throws IOException {
