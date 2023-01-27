8905b25b2a276fa351e295af58e43baa325b82f8
SungCheon Ju <jution113@gmail.com>
2023-01-27 13:13:27 +0900

    [BE Feat] repository 구현

--- server/src/main/java/com/seb_main_002/eventBanner/repository/BannerRepository.java
+++ server/src/main/java/com/seb_main_002/eventBanner/repository/BannerRepository.java
@@ -1,6 +1,6 @@
-package com.seb_main_002.eventImage.repository;
+package com.seb_main_002.eventBanner.repository;
 
-import com.seb_main_002.eventImage.entity.Banner;
+import com.seb_main_002.eventBanner.entity.Banner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BannerRepository extends JpaRepository<Banner, Long> {
