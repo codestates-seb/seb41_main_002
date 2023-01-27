d72a569e07e70199664055a0d21fb374b228509f
rlawodn46465 <rlawodn46465@naver.com>
2023-01-27 22:00:57 +0900

    [FE Feat] 로그아웃 기능 구현

--- client/src/Components/Commons/Header.tsx
+++ client/src/Components/Commons/Header.tsx
@@ -2,43 +2,74 @@ import CartIcon from "../../Icons/CartIcon";
 import UserIcon from "../../Icons/UserIcon";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import "./../Style/header.css";
-import CustomButton from "./Buttons";
 import LogoutIcon from "../../Icons/LogoutIcon";
+import { onLogout } from "../../API/LogoutAPI";
+import "./../Style/header.css";
 
 export default function Header() {
   const [isDropdownClicked, setIsDropdownClicked] = useState(false);
+  const [isCheck, setIsCheck] = useState(false);
 
   const memberId = sessionStorage.getItem("memberId");
 
   const toggleDropdown = () => {
+    setIsCheck(!isCheck);
     setIsDropdownClicked(!isDropdownClicked);
   };
 
   const userLogOut = () => {
-    console.log("ㅎㅇ");
+    onLogout().then(() => {
+      window.location.reload();
+    });
   };
+  
   return (
     <header>
       <nav className="Header_Nav">
         <div className="Nav_Menu">
           <label className="Nav_Btn" htmlFor="Nav_Menu">
-            <input type="checkbox" id="Nav_Menu"/>
+            <input
+              type="checkbox"
+              id="Nav_Menu"
+              checked={isCheck}
+              onChange={toggleDropdown}
+            />
             <span></span>
             <span></span>
             <span></span>
           </label>
+          {isDropdownClicked ? (
+            <ul className="Nav_List">
+              <li onClick={toggleDropdown}>
+                <Link to="/items-list/all">제품</Link>
+              </li>
+              <li>
+                <div className="Nav_Line"></div>
+              </li>
+              <li onClick={toggleDropdown}>
+                <Link to="/items-list/toner">스킨/토너</Link>
+              </li>
+              <li onClick={toggleDropdown}>
+                <Link to="/items-list/cream">크림</Link>
+              </li>
+              <li onClick={toggleDropdown}>
+                <Link to="/items-list/lotion">로션</Link>
+              </li>
+              <li onClick={toggleDropdown}>
+                <Link to="/items-list/cleansing">클렌징</Link>
+              </li>
+              <li onClick={toggleDropdown}>
+                <Link to="/items-list/suncream">선크림</Link>
+              </li>
+              <li>
+                <div className="Nav_Line"></div>
+              </li>
+              <li onClick={toggleDropdown}>
+                <Link to="/items-top-list/all">추천 리스트</Link>
+              </li>
+            </ul>
+          ) : null}
         </div>
-        {/* <ul className="Nav_List">
-          <li className="Nav_Dropdown">
-            <button className="Nav_Dropdown_Button" onClick={toggleDropdown}>
-              제품
-            </button>
-          </li>
-          <li>
-            <Link to="/items-top-list/all">추천 리스트</Link>
-          </li>
-        </ul> */}
         <div className="Header_Logo">
           <Link to="/">L'acier</Link>
         </div>
@@ -48,42 +79,20 @@ export default function Header() {
               <UserIcon name="Header_Icon" />
             </Link>
           ) : (
-            <Link to={`/memberPage/${memberId}`}>
-              <UserIcon name="Header_Icon" />
-            </Link>
+            <>
+              <Link to={`/memberPage/${memberId}`}>
+                <UserIcon name="Header_Icon" />
+              </Link>
+              <Link to={`/members/${memberId}/carts`}>
+                <CartIcon name="Header_Icon" />
+              </Link>
+              <div onClick={userLogOut}>
+                <LogoutIcon name="Header_Icon" />
+              </div>
+            </>
           )}
-          <Link to={`/members/${memberId}/carts`}>
-            <CartIcon name="Header_Icon" />
-          </Link>
-          <div onClick={userLogOut}>
-            <LogoutIcon name="Header_Icon" />
-          </div>
         </div>
       </nav>
-      <div className="Nav_Dropdown_Box">
-        {isDropdownClicked ? (
-          <div className="Nav_Dropdown_Content">
-            <Link to="/items-list/all" onClick={toggleDropdown}>
-              All
-            </Link>
-            <Link to="/items-list/toner" onClick={toggleDropdown}>
-              스킨/토너
-            </Link>
-            <Link to="/items-list/cream" onClick={toggleDropdown}>
-              크림
-            </Link>
-            <Link to="/items-list/lotion" onClick={toggleDropdown}>
-              로션
-            </Link>
-            <Link to="/items-list/cleansing" onClick={toggleDropdown}>
-              클렌징
-            </Link>
-            <Link to="/items-list/suncream" onClick={toggleDropdown}>
-              선크림
-            </Link>
-          </div>
-        ) : null}
-      </div>
     </header>
   );
 }
