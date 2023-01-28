9a707f22ac5852bc5f4648eb8c98978b19b8ba1a
Min <61323917+shim5505@users.noreply.github.com>
2023-01-28 17:55:39 +0900

    Revert "[FE Hotfix] my page"

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import "./globalStyle.css";
-import App from "./App";
-import { Provider } from "react-redux";
-import { store } from './Store/store'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
