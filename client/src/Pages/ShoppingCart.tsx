9552cb5dec795f196ee7ecee0707f555a6f553ca
limdumb <dlaruddls@nate.com>
2023-01-28 15:15:57 +0900

    [FE HOTFIX] 네비게이트 변경

--- client/src/Pages/ShoppingCart.tsx
+++ client/src/Pages/ShoppingCart.tsx
@@ -164,7 +164,7 @@ export default function ShoppingCart() {
             padding="5px"
             onClick={() => {
               pushProductData();
-              navigate("/order/check");
+              navigate("/order/checkout");
             }}
           />
         </div>
