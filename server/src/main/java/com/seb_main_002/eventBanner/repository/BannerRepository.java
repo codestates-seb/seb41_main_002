fc6707a3c8dcb5925ef534b6b31afbdfbfb1bc32
SungCheon Ju <jution113@gmail.com>
2023-01-28 22:54:17 +0900

    [BE Refactor] banner 이미지 검색 로직 수정

--- server/src/main/java/com/seb_main_002/eventBanner/repository/BannerRepository.java
+++ server/src/main/java/com/seb_main_002/eventBanner/repository/BannerRepository.java
@@ -2,7 +2,12 @@ package com.seb_main_002.eventBanner.repository;
 
 import com.seb_main_002.eventBanner.entity.Banner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface BannerRepository extends JpaRepository<Banner, Long> {
+    @Query(value = "SELECT * FROM banner ORDER BY banner_id DESC LIMIT 1", nativeQuery = true)
+    Optional<Banner> findLatestBanner();
 
 }
