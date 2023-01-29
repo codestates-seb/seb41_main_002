c62bc8acb904a75a5a15b2023f1556b4680b88f4
minseob shim <shim5505@gmail.com>
2023-01-30 00:37:10 +0900

    [FE Hotfix]기본 기능 구현

--- client/src/Pages/ResetPw.tsx
+++ client/src/Pages/ResetPw.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { resetMemberPw, ResetPwType } from "../API/ResetPw/ResetPwAPI";
 import CustomButton from "../Components/Commons/Buttons";
 import "./Style/resetPw.css";
 
@@ -9,72 +11,90 @@ const InfoWrapper = styled.div`
   justify-content: space-around;
   align-items: center;
   height: 75px;
-  background-color: var(--dark3);
-  border-bottom: 1px solid white;
-
-  &:last-child {
-    border: none;
-  }
+  border-bottom: 1px solid rgba(255, 255, 255, 0.6);
 `;
+const memberId = Number(sessionStorage.getItem("memberId"));
 
 export default function ResetPw() {
-  const [currentPw, setCurrentPw] = useState<string>("");
-  const [newPw, setNewPw] = useState<string>("");
+  const navigate = useNavigate();
+
+  const [resetPwData, setResetPwData] = useState<ResetPwType>({
+    oldPassword: "",
+    newPassword: "",
+  });
   const [confirmNewPw, setConfirmNewPw] = useState<string>("");
 
-  const handleCurrentPw: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    setCurrentPw(e.target.value);
-  };
-
-  const handleNewPw: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    setNewPw(e.target.value);
+  const handleResetPwData: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { value, name } = e.target;
+    setResetPwData({ ...resetPwData, [name]: value });
   };
 
   const handleConfirmPw: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     setConfirmNewPw(e.target.value);
   };
 
+  const memberResetPw: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+
+    if (confirmNewPw !== resetPwData.newPassword) {
+      alert("새 비밀번호가 일치하지 않습니다.");
+    } else {
+      if (window.confirm("새 비밀번호로 변경합니다.")) {
+        resetMemberPw(memberId, resetPwData);
+      }
+      navigate("/");
+    }
+  };
+
   return (
-    <form className="Reset_Pw_Form">
-      <h1>비밀번호 재설정</h1>
-      <div className="Input_Fields_Wrapper">
-        <InfoWrapper>
-          <div className="Input_Label">기존 비밀번호</div>
-          <input
-            className="Input_Content"
-            type="text"
-            value={currentPw}
-            onChange={handleCurrentPw}
+    <div className="Reset_Pw_Wrapper">
+      <p className="Reset_Pw_Intro">비밀번호를 재설정 합니다.</p>
+      <form className="Reset_Pw_Form" onSubmit={memberResetPw}>
+        <div className="Input_Fields_Wrapper">
+          <InfoWrapper>
+            <div className="Input_Label">기존 비밀번호</div>
+            <input
+              className="Input_Content"
+              type="password"
+              name="oldPassword"
+              value={resetPwData.oldPassword}
+              onChange={handleResetPwData}
+            />
+          </InfoWrapper>
+          <InfoWrapper>
+            <div className="Input_Label">새 비밀번호</div>
+            <input
+              className="Input_Content"
+              type="password"
+              name="newPassword"
+              value={resetPwData.newPassword}
+              onChange={handleResetPwData}
+            />
+          </InfoWrapper>
+          <InfoWrapper>
+            <div className="Input_Label">새 비밀번호 확인</div>
+            <input
+              className="Input_Content"
+              type="password"
+              value={confirmNewPw}
+              onChange={handleConfirmPw}
+            />
+          </InfoWrapper>
+        </div>
+        <div className="Confirm_Button_Wrapper">
+          <CustomButton
+            height="50px"
+            bgColor="transparent"
+            content="비밀번호 재설정"
+            fontColor="gold"
+            padding="5px"
+            width="200px"
+            border="none"
+            fontsize="23px"
+            type="submit"
           />
-        </InfoWrapper>
-        <InfoWrapper>
-          <div className="Input_Label">새 비밀번호</div>
-          <input
-            className="Input_Content"
-            type="text"
-            value={newPw}
-            onChange={handleNewPw}
-          />
-        </InfoWrapper>
-        <InfoWrapper>
-          <div className="Input_Label">새 비밀번호 확인</div>
-          <input
-            className="Input_Content"
-            type="text"
-            value={confirmNewPw}
-            onChange={handleConfirmPw}
-          />
-        </InfoWrapper>
-      </div>
-      <CustomButton
-        bgColor="transparent"
-        content="비밀번호 재설정"
-        fontColor="gold"
-        padding="5px"
-        width="200px"
-        border="none"
-        fontsize="23px"
-      />
-    </form>
+        </div>
+      </form>
+    </div>
   );
 }
