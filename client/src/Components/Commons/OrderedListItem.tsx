d5a4f740899af06d4b5582ace4674b89c8bfff89
rlawodn46465 <rlawodn46465@naver.com>
2023-01-29 16:50:51 +0900

    [FE MISC] 구매 페이지 이미지 정렬

--- client/src/Components/Commons/OrderedListItem.tsx
+++ client/src/Components/Commons/OrderedListItem.tsx
@@ -13,12 +13,16 @@ interface ItemInterface {
 const OrderedListItem = ({ item }: { item: ItemInterface }) => {
   return (
     <div className="Ordered_List_Item_Wrapper">
-      <img src={item.itemImageURL} alt="sample image" />
-      <Link to={`/itemDetail/${item.itemId}`}>
-        <span className="Ordered_List_Item_Title">{item.itemTitle} </span>
-      </Link>
-      <span>수량: {item.itemCount}개 </span>
-      <span>가격: {item.itemTotalPrice}원 </span>
+      <div>
+        <img src={item.itemImageURL} alt="sample image" />
+        <Link to={`/itemDetail/${item.itemId}`}>
+          <span className="Ordered_List_Item_Title">{item.itemTitle} </span>
+        </Link>
+      </div>
+      <div className="Ordered_List_Item_Right">
+        <span>수량: {item.itemCount}개 </span>
+        <span>가격: {item.itemTotalPrice}원 </span>
+      </div>
     </div>
   );
 };
