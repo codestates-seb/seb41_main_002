88c4e90094052dbea8f38f3e9c44a22ea99cd761
bum19 <1996joon@gmail.com>
2023-01-29 19:40:33 +0900

    [BE DEBUG] server에서 시간받아올때 9시간 깎이는 이유알기위해 디버깅용 파일올림./ setTimeZone SeverApplication.java에 추가

--- server/src/main/java/com/seb_main_002/ServerApplication.java
+++ server/src/main/java/com/seb_main_002/ServerApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class ServerApplication {
 
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
