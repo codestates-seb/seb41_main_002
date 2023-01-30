305e9ecaf92c706cdf318f7e2e2a8d463192934b
minseob shim <shim5505@gmail.com>
2023-01-30 13:07:31 +0900

    [FE Hotfix]이벤트 페이지 내부 조정

--- client/src/Pages/EventDetail.tsx
+++ client/src/Pages/EventDetail.tsx
@@ -18,18 +18,16 @@ export default function EventDetail() {
   }, []);
   return (
     <div>
-      <h1 className="Event_Title">{event?.title}</h1>
-      <div className="Event_Posted_Date">{event?.createdAt}</div>
-
-      <div className="Event_Image">
+      <div className="Event_Title_Banner">
         <img src={event?.eventTitleImageURL} alt="event image"></img>
+      </div>
+      <h1 className="Event_Title">{event?.title}</h1>
+      <div className="Event_Term">
+        기간:
+        <span>{event?.createdAt}</span> ~ <span>{event?.endAt}</span>
       </div>
 
       <div>
-        <div className="Event_Term">
-          기간:
-          <span>{event?.createdAt}</span> ~ <span>{event?.endAt}</span>
-        </div>
         <div className="Event_Detail">
           <img src={event?.eventContentImageURL} alt="event image"></img>
         </div>
