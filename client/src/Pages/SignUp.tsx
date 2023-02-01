06139e57db74bee316a9eabff34140d913a31dad
limdumb <dlaruddls@nate.com>
2023-02-01 15:00:44 +0900

    [머지 전용] Branch 생성

--- client/src/Pages/SignUp.tsx
+++ client/src/Pages/SignUp.tsx
@@ -15,7 +15,6 @@ import AccessMenu from "../Components/SignUp/AccessMenu";
 import { error } from "console";
 
 const SignUp = () => {
-  //회원가입 정보
   const [Member, setMember] = useState<MemberType>({
     accountId: "",
     password: "",
